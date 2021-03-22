8918a03311ac4cad267c9d95ab3b4a6fab97cfaf
Dzmitry Harbachou <mitya.gorbachev.2013@mail.ru>
2021-03-22 23:34:54 +0300

    TIK-1 added ticket service.

---
+++ tickets/src/app.ts
@@ -0,0 +1,38 @@
+import express from 'express';
+import 'express-async-errors';
+import {json} from 'body-parser';
+import cookieSession from "cookie-session";
+
+import {errorHandler, NotFoundError, currentUser} from "@shootl/common";
+
+import {indexTicketRouter} from "./routes/index";
+import {createTicketRouter} from "./routes/new";
+import {showTicketRouter} from "./routes/show";
+import {updateTicketRouter} from "./routes/update";
+
+/** @type {boolean} */
+const node_env = process.env.NODE_ENV !== 'test';
+
+const app = express();
+app.set('trust proxy', true);
+app.use(json());
+app.use(
+    cookieSession({
+        signed: false,
+        secure: node_env
+    })
+);
+app.use(currentUser);
+
+app.use(indexTicketRouter);
+app.use(createTicketRouter);
+app.use(showTicketRouter);
+app.use(updateTicketRouter);
+
+app.all('*', async (req, res) => {
+    throw new NotFoundError();
+});
+
+app.use(errorHandler);
+
+export {app};
